52a7a880257c7cd7ff7726061ba046f0c7936348
Vida <alisaghari.awd@gmail.com>
2019-10-03 10:43:21 +0330

    boro tush

---
+++ core/resources/views/user/credit.blade.php
@@ -0,0 +1,42 @@
+@extends("user.master")
+
+@section("content")
+    <div class="content-wrapper">
+        <div class="content-wrapper-before"></div>
+        <div class="content-header row">
+        </div>
+        <div class="content-body">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">افزایش اعتبار (اعتبار شما x تومان)</h4>
+                            <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                            <div class="heading-elements">
+                                <ul class="list-inline mb-0">
+                                    <li>
+                                        <a data-action="collapse"><i class="ft-minus"></i></a>
+                                    </li>
+                                    <li>
+                                        <a data-action="reload"><i class="ft-rotate-cw"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="card-content collapse show">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section("footer")
+
+@endsection
