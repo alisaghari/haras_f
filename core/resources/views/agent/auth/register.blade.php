ed95fa7e850c2f568440d3972772457c845d200f
Vida <alisaghari.awd@gmail.com>
2019-10-13 13:48:02 +0330

    validate code melli and phone and date picker

--- core/resources/views/agent/auth/register.blade.php
+++ core/resources/views/agent/auth/register.blade.php
@@ -66,7 +66,7 @@
                             <div class="card-content">
 
                                 <div class="card-body">
-                                    <form class="form-horizontal" action="{{url("agent/register")}}" method="post" enctype="multipart/form-data" novalidate>
+                                    <form class="form-horizontal" action="{{url("agent/register")}}" method="post" enctype="multipart/form-data" onsubmit="return validateForm()">
                                         @csrf
                                         <fieldset class="form-group position-relative has-icon-left">
                                             <input type="text" class="form-control round"  name="organ_name"  style="font-size: 25px ; text-align: center" id="user-password" placeholder="نام سازمان / نام شرکت" >
@@ -113,7 +113,7 @@
                                         <div class="row">
                                             <div class="col-sm-6">
                                                 <fieldset class="form-group position-relative has-icon-left">
-                                                    <input type="text"  class="form-control round" name="n_code"  value="{{$user->n_code}}"  placeholder="شماره ملی"  id="user-name"  required>
+                                                    <input type="text"  class="form-control round" name="n_code" id="n_code"  value="{{$user->n_code}}"  placeholder="شماره ملی"   required>
                                                     <div class="form-control-position">
                                                         <i class="ft-edit"></i>
                                                     </div>
@@ -226,6 +226,38 @@
         fontSize: 18,
 
     });
+</script>
+<script type="text/javascript">
+    function validateForm()
+    {
+        var meli_code = document.getElementById("n_code").value;
+        if (meli_code.length == 10)
+        {
+            if(meli_code=='1111111111' || meli_code=='2222222222' || meli_code=='3333333333' || meli_code=='4444444444' || meli_code=='5555555555'  || meli_code=='6666666666'  || meli_code=='7777777777'  || meli_code=='8888888888'  || meli_code=='9999999999' )
+            {
+                alert(11);
+            }else{
+                c = parseInt(meli_code.charAt(9));
+                n = parseInt(meli_code.charAt(0))*10 + parseInt(meli_code.charAt(1))*9 + parseInt(meli_code.charAt(2))*8 + parseInt(meli_code.charAt(3))*7 + parseInt(meli_code.charAt(4))*6 + parseInt(meli_code.charAt(5))*5 + parseInt(meli_code.charAt(6))*4 + parseInt(meli_code.charAt(7))*3 + parseInt(meli_code.charAt(8))*2;
+                r = n - parseInt(n/11)*11;
+                if ((r == 0 && r == c) || (r == 1 && c == 1) || (r > 1 && c == 11 - r))
+                {
+                    //  alert('کد ملی صحیح می باشد');
+                }
+                else
+                {
+                    alert('کد ملی وارد شده معتبر نمی باشد');
+                    return false;
+                }
+            }
+        }else{
+            alert('کد ملی وارد شده معتبر نمی باشد');
+            return false;
+        }
+
+    }
+
+
 </script>
 </body>
 <!-- END: Body-->
